83c1fd60e77213834c4f93e3705a7853cce83a19
sami suliman <sami.bco@gmail.com>
2019-09-20 04:04:04 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  SELECT * FROM students 
+  SELECT * FROM students ORDER BY 
 end
 
 def lowest_student_gpa
